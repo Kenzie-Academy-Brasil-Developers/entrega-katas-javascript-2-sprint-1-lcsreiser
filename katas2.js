45769c09f787b81d6217d7243f4835a4e2bb0037
Lucas <lcssreiser@gmail.com>
2021-08-10 15:01:51 -0300

    atividades do kata2 feitos

--- katas2.js
+++ katas2.js
@@ -1,29 +1,48 @@
 // comece a criar a sua função add na linha abaixo
-
+function add(n1,n2){
+    return n1+n2
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
 // comece a criar a sua função multiply na linha abaixo
-
+function multiply(n1,n2){
+    let resultado = 0
+    for(let i = 0; i < n2; i++){
+        resultado = add(n1,resultado)
+    } return resultado
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
+function power(x,n){
+    let resultado = 1
+    for(let i = 0; i < n; i++){
+        resultado *=x
+    } return resultado
+}
 
 
 // descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
 // comece a criar a sua função factorial na linha abaixo
-
+function factorial (n){
+    let resultado = 1
+    for(let i = 1; i <= n; i++){
+        resultado *= i
+    } return resultado
+   
+} 
 
 // descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
 /**
@@ -31,7 +50,12 @@
  */
 
 // crie a função fibonacci
-
+let n = 9
+function fibonacci(n) {
+    if (n <= 1){
+        return n;
+    }return fibonacci(n-1) + fibonacci(n-2);
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
+console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
