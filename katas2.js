2a31fc75058adc77b68007c133e7d65015a22b61
Lucas <lcssreiser@gmail.com>
2021-10-09 17:17:08 -0300

    katas refeito e pronto

--- katas2.js
+++ katas2.js
@@ -12,7 +12,8 @@ function multiply(n1,n2){
     let resultado = 0
     for(let i = 0; i < n2; i++){
         resultado = add(n1,resultado)
-    } return resultado
+    } 
+    return resultado
 }
 
 // descomente a linha seguinte para testar sua função
@@ -23,8 +24,9 @@ console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionand
 function power(x,n){
     let resultado = 1
     for(let i = 0; i < n; i++){
-        resultado *=x
-    } return resultado
+        resultado = multiply(x,resultado)
+    }
+    return resultado
 }
 
 
@@ -35,10 +37,10 @@ console.assert(power(3, 4) === 81, 'A função power não está funcionando como
 // comece a criar a sua função factorial na linha abaixo
 function factorial (n){
     let resultado = 1
-    for(let i = 1; i <= n; i++){
-        resultado *= i
-    } return resultado
-   
+    for(let i = 0; i < n; i++){
+        resultado = multiply((n-i),resultado)
+    }
+    return resultado
 } 
 
 // descomente a linha seguinte para testar sua função
@@ -50,11 +52,22 @@ console.assert(factorial(5) === 120, 'A função factorial não está funcionand
  */
 
 // crie a função fibonacci
-let n = 9
 function fibonacci(n) {
-    if (n <= 1){
-        return n;
-    }return fibonacci(n-1) + fibonacci(n-2);
+    let temp1 = 0
+    let temp2 = 1
+    let resultado = 0
+
+    if(n < 2){
+        return n 
+    }
+    for(let i = 2; i <= n; i++){
+        resultado = add (temp1,temp2)
+
+        resultado = add(temp2,temp1)
+        temp1 = temp2
+        temp2 = resultado
+    }
+    return resultado
 }
 
 // descomente a linha seguinte para testar sua função
